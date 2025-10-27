165702a077758c48ef01348f35e9941a1c857d7d
Haida Houssam <hossamhaida4@gmail.com>
2025-10-27 15:58:48 +0100

    fixed restRepository on billing service

--- billing-service/src/main/java/boss/team/billingservice/repositories/BillRepository.java
+++ billing-service/src/main/java/boss/team/billingservice/repositories/BillRepository.java
@@ -2,6 +2,8 @@ package boss.team.billingservice.repositories;
 
 import boss.team.billingservice.entities.Bill;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface BillRepository extends JpaRepository<Bill,Long> {
 }
--- billing-service/src/main/java/boss/team/billingservice/repositories/ProductItemRepository.java
+++ billing-service/src/main/java/boss/team/billingservice/repositories/ProductItemRepository.java
@@ -2,6 +2,8 @@ package boss.team.billingservice.repositories;
 
 import boss.team.billingservice.entities.ProductItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ProductItemRepository extends JpaRepository<ProductItem,Long> {
 }
